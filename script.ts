919ac7989a9f0abf171e45753534ce437cc54f82
Gloria <ubahgloria16@gmail.com>
2023-06-30 20:57:28 +0100

    Feat: Added number of score and moves

--- script.ts
+++ script.ts
@@ -3,52 +3,68 @@ let sum = 0;
 let isAlive = false;
 let isWinner = false;
 let message = "";
+let moves = 0;
+let score = 0;
 const info: Element = document.querySelector(".info")!;
 const startBtn: Element = document.querySelector("#start-btn")!;
 const cardsEl: Element = document.querySelector("#cards")!;
 const sumEl: Element = document.querySelector("#sum")!;
-const newCardEl: Element = document.querySelector("#newcard-btn")!;
+const newCardBtn: Element = document.querySelector("#newcard-btn")!;
+const movesEl: Element = document.querySelector("#moves")!;
+const scoreEl: Element = document.querySelector("#score")!;
 
 startBtn.addEventListener("click", () => {
-     cards.length !== 2 ? startGame() : null;
+     cards.length !== 2 ? playGame() : null;
 });
 
-newCardEl.addEventListener("click", () => {
+newCardBtn.addEventListener("click", () => {
      getNewCard();
 })
 
 const generateRandomNumbers = () => {
-     const randomNumbers = Math.floor(Math.random() * 13)
+     const randomNumbers = Math.floor(Math.random() * 13);
      return randomNumbers;
 }
 
 const renderGame = () => {
-     let cardTemplate = ""
+     let cardTemplate = "";
      if (sum < 21) {
           message = "Do you want to draw a new card?";
      } else if (sum === 21) {
-          message = "Woohoo!! You won this round!";
           isWinner = true;
+          score += 20;
+          message = "Woohoo!! You won this round!";
      } else {
           message = "You're out of the game!!";
+          score = 0;
+          moves = 0;
           isAlive = false;
           isWinner = false;
      }
      cards.map(card => {
-          cardTemplate += `${card} `
+          cardTemplate += `${card} `;
      });
      cardsEl.innerHTML = `<span>Cards: ${cardTemplate} </span>`;
-     sumEl.innerHTML = `<span>Sum: ${sum}</span>`
+     sumEl.innerHTML = `<span>Sum: ${sum}</span>`;
      info.textContent = message;
+     movesEl.innerHTML = `Moves: ${moves}`
+     scoreEl.innerHTML = `Score: ${score}`
 }
 
 const startGame = () => {
+     console.log(isWinner)
      isAlive = true
      const firstCard = generateRandomNumbers();
      const secondCard = generateRandomNumbers();
      cards = [firstCard, secondCard];
      sum = firstCard + secondCard;
-     renderGame()
+     moves++;
+     score += 5;
+}
+
+const playGame = () => {
+     startGame();
+     renderGame();
 }
 
 const getNewCard = () => {
@@ -56,6 +72,8 @@ const getNewCard = () => {
           const newCard = generateRandomNumbers();
           cards.push(newCard);
           sum += newCard;
-          renderGame()
+          moves++;
+          score += 5;
+          renderGame();
      }
 }
